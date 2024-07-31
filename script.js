ea0a34c509719666364cb0ba46a699feb5055952
aanyasi <99766758+aanyasi@users.noreply.github.com>
2024-07-31 12:20:45 -0700

    Update script.js
    added all the constants for the form on create activity page

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ document.getElementById('activityInputs').addEventListener('submit', function(ev
     event.preventDefault(); // Prevent form from submitting the traditional way
 
     // Get the values from the form
-    const name = document.getElementById('name').value;
-    const age = document.getElementById('age').value;
+    const nActivity = document.getElementById('nActivity').value;
+    const 1date = document.getElementById('1date').value;
+    const Ireccuring = document.getElementById('Ireccuring').value;
+    const wLocation = document.getElementById('wLocation').value;
+    const wProcess = document.getElementById('wProcess').value;
+    const imageUpload = document.getElementById('impageUpload').value;
+    const states = document.getElementById('states').value;
+
 
     // Redirect to a new page with the values as query parameters
     window.location.href = `display.html?name=${encodeURIComponent(name)}&age=${encodeURIComponent(age)}`;
